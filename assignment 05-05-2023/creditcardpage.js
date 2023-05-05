293671c9e2b108c8615e09257cac6a0bc913d719
18Dmartin <112864911+18Dmartin@users.noreply.github.com>
2023-05-05 12:37:25 +0100

    Add files via upload

---
+++ assignment 05-05-2023/creditcardpage.js
@@ -0,0 +1,14 @@
+//connect to input
+let onsub = document.getElementsById("recieve ccn");
+
+
+onsub.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+let creditcnum = document.getElementById("ccn");
+console.log(creditcnum)
+
+})
+
+
+
